afe6ae3c936dadcca2fd7b8a704bc151b5c38e7b
chahd49 <snxwfqll061707@gmail.com>
2025-04-18 13:48:08 -0400

    Done

--- anotherattempt/App.js
+++ anotherattempt/App.js
@@ -3,14 +3,24 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}>
+      <Text>Navbar</Text>
       <StatusBar style="auto" />
     </View>
 
     <View style={styles.top}>
-      <Text>Navbar</Text>
+      <Text>Image/Video</Text>
+      <StatusBar style="auto" />
+    </View>
+
+    <View style={styles.section2}>
+      <Text>Section 2</Text>
+      <StatusBar style="auto" />
+    </View>
+    
+    <View style={styles.footer}>
+      <Text>Footer</Text>
       <StatusBar style="auto" />
     </View>
     </View>
@@ -24,15 +34,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  top: {
-    flex: 1,
     backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+    width: 500
+  },
+  top: {
+    flex: 5,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 500
+  },
+  section2: {
+    flex: 5,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 500
+  },
+  footer: {
+    flex: 1,
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 500
   },
 });
 
